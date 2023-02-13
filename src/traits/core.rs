cfc60aced60b5973bdaf13494ff414831a174d01
Fanatic Provender <fanatic.provender@gmail.com>
2023-02-13 10:49:18 -0500

    CoreExt WIP

--- src/traits/core.rs
+++ src/traits/core.rs
@@ -6,3 +6,14 @@ pub trait Core {
     fn start_loop(&mut self) -> anyhow::Result<&mut Self>;
     fn end_loop(&mut self) -> anyhow::Result<&mut Self>;
 }
+
+trait CoreExt: Core {
+    fn loop_<F>(&mut self, f: F) -> anyhow::Result<&mut Self>
+    where
+        F: FnOnce(&mut Self) -> anyhow::Result<&mut Self>,
+    {
+        self.start_loop()?;
+        f(self)?;
+        self.end_loop()
+    }
+}
