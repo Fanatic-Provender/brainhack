ffe27c993c482932c75e56d00396ac188c5d1fce
Fanatic Provender <fanatic.provender@gmail.com>
2023-02-24 13:59:55 -0500

    Add is_le_zero

--- src/traits/arith.rs
+++ src/traits/arith.rs
@@ -72,6 +72,20 @@ pub trait Arith: Logic {
                 |s| s.is_lt_zero_move(word, dest, temp_1, temp_2),
             )
     }
+    fn is_le_zero(
+        &mut self,
+        word: Word,
+        dest: Pos,
+        temp_1: Pos,
+        temp_2: Pos,
+        temp_3: Pos,
+        temp_4: Pos,
+        temp_5: Pos,
+        temp_6: Pos,
+    ) -> anyhow::Result<&mut Self> {
+        self.copy_word(word, &[(temp_1, temp_2)], dest)?
+            .is_le_zero_move((temp_1, temp_2), dest, temp_3, temp_4, temp_5, temp_6)
+    }
 
     fn is_ge_zero_move(
         &mut self,
@@ -322,6 +336,37 @@ mod tests {
         test::compare_tape(coder.writer(), &[12, 34], 0, &[0, 0, 0, 0, 0, 0, 0], 0);
         test::compare_tape(coder.writer(), &[200, 100], 0, &[0, 0, 1, 0, 0, 0, 0], 0);
         test::compare_tape(coder.writer(), &[128, 0], 0, &[0, 0, 1, 0, 0, 0, 0], 0);
+        Ok(())
+    }
+
+    #[test]
+    fn is_le_zero() -> anyhow::Result<()> {
+        let mut coder = Coder::new(vec![]);
+        coder.is_le_zero((0, 1), 2, 3, 4, 5, 6, 7, 8)?.seek(0)?;
+
+        test::compare_tape(coder.writer(), &[0, 0], 0, &[0, 0, 1, 0, 0, 0, 0, 0, 0], 0);
+        test::compare_tape(coder.writer(), &[0, 5], 0, &[0, 5, 0, 0, 0, 0, 0, 0, 0], 0);
+        test::compare_tape(
+            coder.writer(),
+            &[12, 34],
+            0,
+            &[12, 34, 0, 0, 0, 0, 0, 0, 0],
+            0,
+        );
+        test::compare_tape(
+            coder.writer(),
+            &[200, 100],
+            0,
+            &[200, 100, 1, 0, 0, 0, 0, 0, 0],
+            0,
+        );
+        test::compare_tape(
+            coder.writer(),
+            &[128, 0],
+            0,
+            &[128, 0, 1, 0, 0, 0, 0, 0, 0],
+            0,
+        );
         Ok(())
     }
 
