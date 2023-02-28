11896c104821a52c540debc458d8f8c178e5d282
Fanatic Provender <fanatic.provender@gmail.com>
2023-02-28 12:59:56 -0500

    Temporary

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod interpreter;
 use interpreter::{Parser, Interpreter};
 
 fn main() {
-    let program = Parser::from_file("src/add.bf".to_owned())
+    let program = Parser::from_file("src/examples/add.bf".to_owned())
         .unwrap()
         .optimized_parse();
     let mut interpreter = Interpreter::new(program);
