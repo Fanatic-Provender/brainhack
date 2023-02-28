45ae22ed434d47e6f33b9c550a3876733bf5170b
L0ad1n6 <altan.mehmet.unver@gmail.com>
2023-02-28 15:25:01 -0500

    Benchmarking

---
+++ benches/add16.rs
@@ -0,0 +1,18 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+use brainhack::interpreter::{Parser, Interpreter};
+
+pub fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("16 Bit Add", 
+    |b| b.iter(
+        || {
+            let program = Parser::from_file("test/add16.bf".to_owned())
+                .unwrap()
+                .optimized_parse();
+            let mut interpreter = Interpreter::new(program);
+            interpreter.eval().unwrap();
+        }
+    ));
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
