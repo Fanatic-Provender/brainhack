e0320e47737da992fcc23d4ed435dabf27785b2e
Fanatic Provender <fanatic.provender@gmail.com>
2023-03-01 19:47:59 -0500

    Add binary_or_cell_move

--- src/traits/binary.rs
+++ src/traits/binary.rs
@@ -104,6 +104,31 @@ pub trait Binary: Arith {
         self.binary_and_cell(a.0, b.0, dest.0, temp)?
             .binary_and_cell(a.1, b.1, dest.1, temp)
     }
+
+    fn binary_or_cell_move(
+        &mut self,
+        a: Pos,
+        b: Pos,
+        dest: Pos,
+        temp: [Pos; 7],
+    ) -> anyhow::Result<&mut Self> {
+        let mul = temp[0];
+        self.seek(mul)?.inc_val()?;
+        for _ in 0..8 {
+            self.div_mod_two_cell(a, temp[1], [temp[3], temp[4], temp[5], temp[6]])?
+                .div_mod_two_cell(b, temp[2], [temp[3], temp[4], temp[5], temp[6]])?
+                .logical_or_move(a, b, temp[3])?
+                .move_cell(temp[1], &[a])?
+                .move_cell(temp[2], &[b])?
+                .if_move(temp[3], |s| {
+                    // relies on adding semantics of copy_cell
+                    s.copy_cell(mul, &[dest], temp[4])
+                })?
+                .mul_two_move_cell(mul, temp[3])?
+                .move_cell(temp[3], &[mul])?;
+        }
+        self.clear_cell(&[mul])
+    }
 }
 impl<T: Arith> Binary for T {}
 
@@ -304,6 +329,45 @@ mod tests {
             &[31, 41, 59, 26],
             0,
             &[31, 41, 59, 26, 31 & 59, 41 & 26, 0, 0, 0, 0, 0, 0, 0, 0],
+            0,
+        );
+
+        Ok(())
+    }
+
+    #[test]
+    fn binary_or_cell_move() -> anyhow::Result<()> {
+        let mut coder = Coder::new(vec![]);
+        coder
+            .binary_or_cell_move(0, 1, 2, [3, 4, 5, 6, 7, 8, 9])?
+            .seek(0)?;
+
+        test::compare_tape(
+            coder.writer(),
+            &[0, 0],
+            0,
+            &[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            0,
+        );
+        test::compare_tape(
+            coder.writer(),
+            &[0, 42],
+            0,
+            &[0, 0, 42, 0, 0, 0, 0, 0, 0, 0],
+            0,
+        );
+        test::compare_tape(
+            coder.writer(),
+            &[31, 41],
+            0,
+            &[0, 0, 31 | 41, 0, 0, 0, 0, 0, 0, 0],
+            0,
+        );
+        test::compare_tape(
+            coder.writer(),
+            &[215, 148],
+            0,
+            &[0, 0, 215 | 148, 0, 0, 0, 0, 0, 0, 0],
             0,
         );
 
